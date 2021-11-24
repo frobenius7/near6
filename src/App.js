c0d3b6ee36ef4f04c4566713fed96257799918c2
frobenius7 <vetb@me.com>
2021-11-24 16:02:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -158,9 +158,9 @@ const App = ({ contract, currentUser, nearConfig, wallet }) => {
             <div style={{ flex: 1 }}>
               <MintForm onMintSubmit={onMintSubmit} currentUser={currentUser} />
             </div>
-            <div style={{ flex: 1 }}>
+            <p><div style={{ flex: 1 }}>
               <TransferForm onTransferSubmit={onTransferSubmit} currentUser={currentUser} balance={balance} />
-            </div>
+            </div></p>
           </div>
         </div>
         : <SignIn />
