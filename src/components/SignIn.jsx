7ab3800a9e39cb669974b42fb50bf94a7f7ea90e
frobenius7 <vetb@me.com>
2021-11-24 15:41:19 +0300

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-// import banner from '../assets/big-spaceman.png';
-import banner from '../assets/beard.svg';
+
 
 export default function SignIn() {
   return (
-    <div className="image-container">
-      <img src={banner} style={{width: '50%' }} alt="Near to the moon" />
+    <div className="test-container">
+      <h1>NEARvember challenge 5 - NFT minting frontent!</h1>
     </div>
-        <h1>NEARvember challenge 5 - NFT minting frontent!</h1>
-        <p>
-          Please sign in.
-        </p>
-
+        
   );
 }
