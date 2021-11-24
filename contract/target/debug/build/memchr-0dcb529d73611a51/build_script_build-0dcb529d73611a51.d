207bc3d9d00ccdd6fa8b4d50c35eefd911e25535
frobenius7 <vetb@me.com>
2021-11-24 15:14:35 +0300

    xz

--- contract/target/debug/build/memchr-0dcb529d73611a51/build_script_build-0dcb529d73611a51.d
+++
@@ -1,5 +0,0 @@
-/Users/vb/dev/near5challenge/contract/target/debug/build/memchr-0dcb529d73611a51/build_script_build-0dcb529d73611a51: /Users/vb/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.3.4/build.rs
-
-/Users/vb/dev/near5challenge/contract/target/debug/build/memchr-0dcb529d73611a51/build_script_build-0dcb529d73611a51.d: /Users/vb/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.3.4/build.rs
-
-/Users/vb/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.3.4/build.rs:
