980065ff9b367cc2900a2151ffd203b97843a7a7
frobenius7 <vetb@me.com>
2021-11-24 15:43:08 +0300

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function SignIn() {
   return (
     <div className="test-container">
-      <h1>NEARvember challenge 5 - NFT minting frontent!</h1>
+      <h1>NEARvember challenge 6 - FT mint and transfer!</h1>
     </div>
         
   );
